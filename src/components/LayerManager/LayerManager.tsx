ce7f8ed520537117b1ca5df9e606d05a3845b44b
Jean-MarcCheynier <jmcheynier@gmail.com>
2024-10-30 00:57:07 +0100

    Test animation with tailwind

--- src/components/LayerManager/LayerManager.tsx
+++ src/components/LayerManager/LayerManager.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import React, { FC, PropsWithChildren } from "react";
+import React, { FC, PropsWithChildren, ReactNode } from "react";
 import styles from "./LayerManager.module.css";
 import { LayerProps, LayerWithContext } from "@/src/components/Layer/Layer";
-import { LayerContextProvider } from "../LayerContextProvider";
+import {
+  LayerContextMapProps,
+  LayerId,
+  useLayerContext,
+} from "../LayerContextProvider";
 import LayerController from "../LayerController/LayerController";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
@@ -14,25 +18,57 @@ export type LayerManagerComposition = {
   Controller: typeof LayerController;
 };
 
+const getActiveLayer = (layerContext: LayerContextMapProps) => {
+  return Object.entries(layerContext).find(([, { active }]) => active)?.[0];
+};
+
+const baseAnimation =
+  "transition transform ease-linear duration-1000 motion-reduce:transition-none";
+const classFirstLayer = `${baseAnimation} translate-y-20 -translate-x-20 `;
+const classSecondLayer = `${baseAnimation} translate-y-10 -translate-x-14`;
+const classThirdLayer = `${baseAnimation} translate-y-0 translate-x-1`;
+const classFourthLayer = `${baseAnimation} translate-y-10 translate-x-14`;
+const classFifthLayer = `${baseAnimation} translate-y-20 translate-x-20`;
+
 export const LayerManager: FC<PropsWithChildren<LayerManagerProps>> &
   LayerManagerComposition = ({ children }) => {
-  return (
-    <LayerContextProvider>
-      <div className={styles.LayerManager} data-testid="LayerManager">
-        {React.Children.map(children, (child) => {
-          if (React.isValidElement(child)) {
-            if (child.type === LayerWithContext) {
-              const props = child.props as LayerProps;
+  const layerContext = useLayerContext();
+  const activeLayer = getActiveLayer(layerContext);
 
-              return <LayerWithContext {...props} key={props.id} />;
-            }
-            if (child.type === LayerController) {
-              return child;
-            }
-          }
-        })}
+  const layers: ReactNode[] = [];
+  const layerControl: ReactNode[] = [];
+
+  React.Children.forEach(children, (child) => {
+    if (React.isValidElement(child)) {
+      if (child.type === LayerWithContext) {
+        const props = child.props as LayerProps;
+
+        layers.push(<LayerWithContext {...props} key={props.id} />);
+      }
+      if (child.type === LayerController) {
+        layerControl.push(child);
+      }
+    }
+  });
+
+  return (
+    <>
+      <div className="width-full flex justify-center justify-items-center">
+        {layerControl}
       </div>
-    </LayerContextProvider>
+      <div
+        className={`${styles.LayerManager} 
+        ${activeLayer === LayerId.Biography && classFirstLayer}
+        ${activeLayer === LayerId.Contact && classSecondLayer}
+        ${activeLayer === LayerId.Kids && classThirdLayer}
+        ${activeLayer === LayerId.Projects && classFourthLayer}
+        ${activeLayer === LayerId.TextAndDrafts && classFifthLayer}
+        `}
+        data-testid="LayerManager"
+      >
+        {layers}
+      </div>
+    </>
   );
 };
 
