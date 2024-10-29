de827b0a4f35e21921a785d0356ec17338276bd6
Jean-MarcCheynier <jmcheynier@gmail.com>
2024-10-29 18:37:39 +0100

    Remove test files

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import Layer from "@/src/components/LayerManager/LayerManager";
+import { LayerManager } from "@/src/components/LayerManager/LayerManager";
 
 export default function Home() {
   return (
     <>
-      <Layer>
-        <Layer.Item id="0" priority src="/0.png" alt="Layer 0" />
-        <Layer.Item id="1" src="/bio.png" alt="Layer 0" />
-        <Layer.Item id="2" src="/contact.png" alt="Layer 0" />
-        <Layer.Item id="3" src="/kids.png" alt="Layer 0" />
-        <Layer.Item id="4" src="/projects.png" alt="Layer 0" />
-        <Layer.Controller />
-      </Layer>
+      <LayerManager>
+        <LayerManager.Item id="0" priority src="/0.png" alt="Layer 0" />
+        <LayerManager.Item id="1" src="/bio.png" alt="Layer 0" />
+        <LayerManager.Item id="2" src="/contact.png" alt="Layer 0" />
+        <LayerManager.Item id="3" src="/kids.png" alt="Layer 0" />
+        <LayerManager.Item id="4" src="/projects.png" alt="Layer 0" />
+        <LayerManager.Controller />
+      </LayerManager>
     </>
   );
 }
